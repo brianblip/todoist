a4b659fd9f105ea439f25de520436b97707b625a
brianblip <pradojohnbrian@gmail.com>
2024-08-14 16:41:44 +0800

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export default function Home() {
+  return (
+    <div
+      className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable
+      )}
+    >
+      <h1>Hello WOrld</h1>
+    </div>
+  );
+}
